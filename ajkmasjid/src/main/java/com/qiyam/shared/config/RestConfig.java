cae28af2e81ae7584b225ff9d409a5b13e1a6953
Afiq <afiqtechno96@gmail.com>
2026-06-21 22:23:18 +0800

    fix error when running project

--- ajkmasjid/src/main/java/com/qiyam/shared/config/RestConfig.java
+++ ajkmasjid/src/main/java/com/qiyam/shared/config/RestConfig.java
@@ -1,13 +1,19 @@
 package com.qiyam.shared.config;
 
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestConfig {
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .connectTimeout(Duration.ofSeconds(10))
+                .readTimeout(Duration.ofSeconds(30))
+                .build();
     }
 }
