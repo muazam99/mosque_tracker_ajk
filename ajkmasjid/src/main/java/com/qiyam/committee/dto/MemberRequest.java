795f0b8a9ca33677d5af02af6da51c0d6308926c
Afiq <afiqtechno96@gmail.com>
2026-07-29 10:02:16 +0800

    add logoit functionality

--- ajkmasjid/src/main/java/com/qiyam/committee/dto/MemberRequest.java
+++ ajkmasjid/src/main/java/com/qiyam/committee/dto/MemberRequest.java
@@ -1,14 +1,18 @@
 package com.qiyam.committee.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import java.time.LocalDate;
 
 @Data
 public class MemberRequest {
+    @JsonProperty("user_id")
     private String userId;
+    @JsonProperty("committee_role_id")
     private Long committeeRoleId;
     private LocalDate appointmentStart;
     private LocalDate appointmentEnd;
     private String status;
+    @JsonProperty("appointed_by")
     private String appointedBy;
 }
